a72734f6d2cff80760e52054ba112cdbbe8b0705
anespinosa <anespinosa@uc.cl>
2021-01-31 22:03:21 +0000

    Documentation Southern Women

---
+++ R/southern_women.R
@@ -0,0 +1,14 @@
+#' Southern Women
+#'
+#' This data was collected in 1930 for ethnographic studies by Davis, Gardner and Gardner. It consists of two incident matrices. The first matrix is a group of 18 women attending 14 events. The second matrix is a different group of 6 women attending 9 events.
+#'
+#' @format A 6 X 9 incident matrix of women in events.
+#'
+#' @source Davis, A., Gardner, B. B. and M. R. Gardner (1941). Deep South. The University of Chicago Press, Chicago.
+#'
+#' @usage data(southern_women)
+#'
+#' @docType data
+#'
+#'
+"southern_women"
