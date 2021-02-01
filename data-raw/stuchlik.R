314cf95a37949e9946b93acd96cd8cf5b745a528
FranciscaOrtizRuiz <franortizruiz@gmail.com>
2021-02-01 19:53:16 +0000

    Add kin_mapuche

---
+++ data-raw/stuchlik.R
@@ -0,0 +1,104 @@
+
+##### Mapuches in Coipuco, Chile (1968-70) ####
+
+##### I. Kindship and intensity of it ####
+
+#matrix1 <- matrix(c(0), byrow=TRUE, ncol=28)
+#label1 <- c("MANQUEPI_HUENCHULEO","HUENCHUMAN_CARLOS","HUENCHUAN_MANQUEPI",
+#            "CAYUL_PAINE","CAYUL_JUAN","TRANAMIL_JUAN",
+#            "TRANAMIL_DANIEL", "MILLAREAL_ANTONIO", "MILLAREAL_AGUSTIN",
+#            "MILLAREAL_JULIO", "CURIHUAL_PAINE", "CURIHUAL_JUAN",
+#            "ALCAPAN_JUAN", "MILLAR_LUIS", "BARRA_CARLOS",
+#            "CAYUL_CARMEN", "BARRA_AUGUSTINA", "BARRA_SERAFINA",
+#            "CAYUL_AGUSTIN", "CAYUL_SEGUNDO", "CAYUL_MANUEL",
+#            "CAYUL_ALARCON", "CAYUL_FRANCISCO", "CAYUL_ANTONIO",
+#            "CAYUL_COCHE", "CARIQUEO_DOMINGO", "CAYUL_JACINTO",
+#            "CURIHUAL HIPOLITO")
+#
+#colnames(matrix1) <- label1
+#rownames(matrix1) <- label1
+#
+#reservations1 <- c("I", "I", "I", "II","II",
+#                   "II", "II", "II", "II", "II",
+#                   "II", "II", "III", "III", "III",
+#                   "IV", "IV", "V", "V", "V",
+#                   "V", "V", "V", "VI", "VI",
+#                   "VI", "VI", "VI")
+
+intensity <- matrix(c(0,2,2,2,2,2,3,2,2,2,4,0,0,0,3,2,2,3,0,0,0,3,3,0,0,0,0,0,
+                    2,0,1,2,0,2,2,2,2,2,4,0,3,0,2,2,2,0,3,3,2,2,3,3,2,2,2,0,
+                    2,1,0,2,2,2,2,2,2,2,4,2,2,2,2,2,2,2,2,0,2,2,3,3,2,2,2,0,
+                    2,2,2,0,1,2,2,1,1,3,3,3,2,2,2,1,1,2,2,2,2,3,2,2,2,2,2,2,
+                    2,0,2,1,0,3,3,4,0,2,2,2,0,2,3,2,2,0,3,0,2,0,2,2,2,0,2,0,
+                    2,2,2,2,2,0,1,2,2,3,2,2,2,2,1,4,4,0,0,0,0,0,0,0,0,0,0,0,
+                    2,2,2,2,3,1,0,2,2,2,2,2,2,2,1,4,4,2,2,2,2,2,2,2,2,3,3,3,
+                    2,2,2,1,2,2,2,0,1,2,2,2,4,2,2,2,2,2,1,1,1,2,2,3,2,3,3,3,
+                    2,2,2,1,4,2,2,1,0,2,2,2,2,0,2,0,0,0,2,2,2,2,2,2,2,3,2,0,
+                    2,2,2,3,2,3,2,2,2,0,2,2,2,2,3,0,2,3,2,0,3,2,2,0,0,0,3,0,
+                    4,4,4,3,2,2,2,2,2,2,0,1,3,1,3,2,2,0,2,3,2,2,2,3,2,0,3,2,
+                    4,0,2,3,2,2,2,2,2,2,1,0,3,1,2,2,0,0,0,0,0,0,2,3,0,0,3,2,
+                    0,3,2,2,0,2,2,4,2,2,3,3,0,2,1,2,2,3,2,0,3,2,2,3,3,0,0,0,
+                    0,0,2,2,3,2,2,2,2,2,1,1,2,0,2,2,2,2,3,2,2,3,2,3,3,0,0,0,
+                    3,2,2,2,3,1,1,2,3,3,2,2,1,2,0,1,1,3,2,3,0,3,2,2,3,0,3,3,
+                    2,2,3,2,2,2,2,2,2,3,2,2,2,2,1,0,1,4,2,0,2,2,2,2,2,2,2,3,
+                    2,2,3,2,2,2,2,2,2,2,0,0,2,2,1,1,0,2,2,2,2,2,2,2,3,2,3,3,
+                    0,0,2,2,0,0,0,0,3,3,0,0,3,2,3,4,2,0,4,0,0,2,2,0,0,0,0,0,
+                    3,3,2,2,2,3,3,1,0,2,0,0,2,2,2,2,2,4,0,2,1,1,2,1,1,2,2,0,
+                    0,0,0,0,0,0,0,2,2,0,0,0,0,0,0,0,4,0,2,0,1,2,2,0,0,0,0,0,
+                    0,0,0,0,0,0,0,1,2,3,0,0,0,0,0,2,2,2,1,1,0,1,2,2,2,2,2,2,
+                    3,3,2,2,2,2,0,2,2,2,0,0,2,3,3,2,2,4,1,4,1,0,2,2,2,2,2,0,
+                    3,2,2,2,2,2,2,2,2,2,2,2,2,1,0,2,2,2,2,2,2,2,0,2,2,3,2,0,
+                    0,0,0,0,0,2,0,0,2,0,0,0,0,0,3,3,2,0,1,2,2,2,2,0,1,2,2,0,
+                    0,2,2,2,2,0,2,2,2,0,2,0,3,3,3,2,3,0,1,0,2,2,2,1,0,2,2,3,
+                    0,2,2,2,0,0,3,3,3,0,0,0,0,0,0,2,2,0,2,0,2,2,3,2,2,0,1,3,
+                    0,0,0,0,3,2,2,2,2,3,3,3,0,0,0,2,2,0,2,2,2,2,2,2,2,1,0,3,
+                    0,0,0,2,0,0,3,3,0,0,2,2,0,0,3,3,3,0,0,0,2,0,0,0,3,3,3,0), byrow=TRUE, ncol=28)
+label2 <- c("MANQUEPI_HUENCHULEO","HUENCHUMAN_CARLOS","HUENCHUAN_MANQUEPI",
+            "CAYUL_PAINE","CAYUL_JUAN","TRANAMIL_JUAN",
+            "TRANAMIL_DANIEL", "MILLAREAL_ANTONIO", "MILLAREAL_AGUSTIN",
+            "MILLAREAL_JULIO", "CURIHUAL_PAINE", "CURIHUAL_JUAN",
+            "ALCAPAN_JUAN", "MILLAR_LUIS", "BARRA_CARLOS",
+            "CAYUL_CARMEN", "BARRA_AUGUSTINA", "BARRA_SERAFINA",
+            "CAYUL_AGUSTIN", "CAYUL_SEGUNDO", "CAYUL_MANUEL",
+            "CAYUL_ALARCON", "CAYUL_FRANCISCO", "CAYUL_ANTONIO",
+            "CAYUL_COCHE", "CARIQUEO_DOMINGO", "CAYUL_JACINTO",
+            "CURIHUAL HIPOLITO")
+colnames(intensity) <- label2
+rownames(intensity) <- label2
+
+reservations <- c("I", "I", "I", "II","II",
+                   "II", "II", "II", "II", "II",
+                   "II", "II", "III", "III", "III",
+                   "IV", "IV", "V", "V", "V",
+                   "V", "V", "V", "VI", "VI",
+                   "VI", "VI", "VI")
+
+kin_mapuche <- list(intensity=intensity,
+                    reservations=reservations)
+
+usethis::use_data(kin_mapuche, overwrite = TRUE)
+
+
+##### II. Position of authority and its relation to genealogical and economic position ####
+
+
+
+##### III. Daily activities ####
+
+
+
+##### IV. Labor sphere  ####
+
+
+
+##### V. Mutual participation in Mingaco  ####
+
+
+
+##### VI. Vuelta mano and informal help between them ####
+
+
+
+##### VII. Formalized friendship relations and their locations ####
+
+
