bd35245ff4c53568a79292bc22593ecb2118c1ac
anespinosa <anespinosa@uc.cl>
2021-02-01 17:40:13 +0000

    Western Electric Company

---
+++ R/bank_room.R
@@ -0,0 +1,25 @@
+#' Western Electric Company
+#'
+#' This matrices are part of a study that was conducted at the Hawthorne Works of the Western Electric Company in Chicago that began in 1927 until 1932. From the so called "First Harvard School of Networks" (Freeman, 2004).
+#' Through observational and interview material was explored the degree and kind of social participation of each individual in the Bank Wiring Observation Room.
+#'
+#' @format A list of matrices of the relationships between the wiremen (W), inspectors (I) and soldermen (S) of the company
+#' \describe{
+#'     \item{games}{Observation of join participation in games activities}
+#'     \item{controversies}{Men who joined in the controversies and those with whom they participated}
+#'     \item{job}{Person who initiated the request to trade jobs to the person who accepted the request}
+#'     \item{help}{Helping one another from helper to the person helped}
+#'     \item{friendship}{Friendships between the workers}
+#'     \item{antagonism}{Antagonisms between the workers}
+#'     \item{clique}{Informal organization of the group of workmen. "A" is "The group located in front of the room" and B is "the group located in the back of the room", NC is "not clear", and O "Outsiders"}
+#' }
+#'
+#' @source Roethlisberger, Fritz Jules and Dickson, William John (1939). Management and the worker. An Account of a Research Program Conducted by the Western Electric Company, Hawthorne Works, Chicago. Harvard University Press: Cambridge, Massachusetts.
+#' @source Freeman, Linton C. (2004). The Development of Social Network Analysis. A Study in the Sociology of Science. Empirical Press, Vancouver, BC Canada
+#'
+#' @usage data(bank_room)
+#'
+#' @docType data
+#'
+#'
+"bank_room"
