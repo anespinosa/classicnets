4fc88a27193f6d7660d5e5c21e9f822b1c61a863
FranciscaOrtizRuiz <franortizruiz@gmail.com>
2021-02-04 14:51:26 +0000

    add informalhelp_mapuche

---
+++ R/informalhelp_mapuche.R
@@ -0,0 +1,25 @@
+#' Mapuches in Coipuco
+#'
+#' This matrices are part of a study that was conducted at Coipuco, in Chile, Southamerica, between 1968 and 1970. The data was collected by Milan Stuchlik.
+#' It has been recognized as one of the first studies in Chile and latinamerica using social network analysis (Ortiz and Espinosa-Rada, forthcoming).
+#' The complete work was published as a book by Stuchlik (1976), which described in more detailed the complete etnography.
+#'
+#' @references Ortiz, Francisca and Espinosa-Rada, Alejandro (forthcoming). Ciencias de redes sociales en America Latina: enfasis y nuevas directrices en el campo de las ciencias sociales. In Ortiz, Francisca and Espinosa-Rada, Alejandro (eds.) El analisis de redes sociales en Latinamerica. Madrid, Spain: Editorial CIS.
+#'
+#' @format A list of matrices of the relationships and other activities shared by the Mapuche community in Coipuco.
+#' \describe{
+#'     \item{informalhelp}{Vuelta mano and informal help between coupucans.
+#'                         0: no information or no informal between them.
+#'                         1: vuelta mano: lasting or frequently expressed relationship.
+#'                         2: lasting mutual help of two near relatives.
+#'                         3: informal help: at least one instance observed.}
+#' }
+#'
+#' @source Stuchlik, Milan (1976). The life on half share. Mechanisms of Social Recruitment among the Mapuche of Southern Chile. London: C. Hurst & Company.
+#'
+#' @usage data(informalhelp_mapuche)
+#'
+#' @docType data
+#'
+#'
+"informalhelp_mapuche"
