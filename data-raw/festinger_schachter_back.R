419064495a8ce3acda6b262dbf523a89566c470e
FranciscaOrtizRuiz <franortizruiz@gmail.com>
2021-02-07 23:22:16 +0000

    add courts

--- data-raw/festinger_schachter_back.R
+++ data-raw/festinger_schachter_back.R
@@ -21,7 +21,7 @@ tolman <- matrix(c(0,0,0,0,0,0,0,0,0,0,0,0,0,
 labelcol <- c("givento1", "givento2", "givento3", "givento4", "givento5",
               "givento6", "givento7", "givento8", "givento9", "givento10",
               "givento11", "givento12", "givento13")
-labelrow <- c("givenby1", "givenby2", "givenby3", "givenby4", "given5",
+labelrow <- c("givenby1", "givenby2", "givenby3", "givenby4", "givenby5",
               "givenby6", "givenby7", "givenby8", "givenby9", "givenby10",
               "givenby11", "givenby12", "givenby13")
 
@@ -55,4 +55,3 @@ rownames(howe) <- labelrow
 howe_court <- list(howe=howe)
 
 usethis::use_data(howe_court, overwrite = TRUE)
-
