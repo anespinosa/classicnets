6c0667d4b4f23069ce2c8fb685b7694a9ea0ebd1
FranciscaOrtizRuiz <franortizruiz@gmail.com>
2021-02-08 15:18:04 +0000

    add cornerboys descrp

---
+++ R/corner_boys.R
@@ -0,0 +1,32 @@
+#' Corner Boys
+#'
+#' This data was as a result of a etnography done in an italian american slum of
+#' Corneville, in Boston, USA.
+#'
+#' @format One 13 X 13 matrix (The Nortons), and another one of 8 X 8
+#' (Angelo's boys).
+#' \describe{
+#'     \item{thenortons}{A 13 X 13 matrix of the corner boys in the nortons
+#'     gans. The ones who send the tie are in row, and the ones who received
+#'     the ties are in columns.}
+#'     \item{statusnortons}{The status of each corener boys. "1" is higher, and
+#'      "7" is the lowest.}
+#'     \item{angelo}{A 8 X 8 matrix of corner boys included in the Angelo's
+#'     boys. The ones who send the tie are in row, and the ones who received
+#'     the ties are in columns.}
+#'     \item{statusangelo}{The status of each corener boys. "1" is higher, and
+#'      "5" is the lowest.}
+#'     \item{freqangelo}{The fraquency of the corner boys, where "1" is
+#'     "frequent", and "2" is for those "infrequently present".}
+#'     }
+#'
+#' @source Whyte, Willian Foote (1993[1943]). Street corner society. The social
+#' structure of an italian slum. Chicago, United States: The University
+#' of Chicago Press.
+#'
+#' @usage data(corner_boys)
+#'
+#' @docType data
+#'
+#'
+"corner_boys"
