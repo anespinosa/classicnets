ddbab23127f7bacf9e3911717bc8c6e9e70292bc
FranciscaOrtizRuiz <franortizruiz@gmail.com>
2021-02-08 16:31:35 +0000

    add date

--- R/corner_boys.R
+++ R/corner_boys.R
@@ -1,19 +1,19 @@
 #' Corner Boys
 #'
 #' This data was as a result of a etnography done in an italian american slum of
-#' Corneville, in Boston, USA.
+#' Corneville, in Boston, USA. Data collected around 1937-40.
 #'
 #' @format One 13 X 13 matrix (The Nortons), and another one of 8 X 8
 #' (Angelo's boys).
 #' \describe{
 #'     \item{thenortons}{A 13 X 13 matrix of the corner boys in the nortons
 #'     gans. The ones who send the tie are in row, and the ones who received
-#'     the ties are in columns.}
+#'     the ties are in columns. It represent the line of influence between them.}
 #'     \item{statusnortons}{The status of each corener boys. "1" is higher, and
 #'      "7" is the lowest.}
 #'     \item{angelo}{A 8 X 8 matrix of corner boys included in the Angelo's
 #'     boys. The ones who send the tie are in row, and the ones who received
-#'     the ties are in columns.}
+#'     the ties are in columns. It represent the line of influence between them.}
 #'     \item{statusangelo}{The status of each corener boys. "1" is higher, and
 #'      "5" is the lowest.}
 #'     \item{freqangelo}{The fraquency of the corner boys, where "1" is
