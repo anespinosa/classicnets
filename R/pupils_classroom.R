bfae76786df1c90a83965bd69b4366149e9e864e
anespinosa <anespinosa@uc.cl>
2021-02-09 22:45:20 +0000

    Sociometry J Moreno

---
+++ R/pupils_classroom.R
@@ -0,0 +1,26 @@
+#' Friendship choices among pupils
+#'
+#' This data was collected by Jacob L. Moreno and Helen Jennings in 19XX... Each pupils were asked "Who wants to be sitting next to whom?"
+#' Each child could choose two others...
+#'
+#' @format A list of square directed matrices
+#'
+#' @source Moreno, Jacob L. (1934). Who Shall Survive? Washington, DC: Nervous and Mental Disease Publishing Company.
+#'
+#' @usage data(pupils_classroom)
+#' \describe{
+#'     \item{friendship_1th}{1st Grade}
+#'     \item{friendship_2th}{2nd Grade}
+#'     \item{friendship_3th}{3rd Grade}
+#'     \item{friendship_4th}{4th Grade}
+#'     \item{friendship_5th}{5th Grade}
+#'     \item{friendship_6th}{6th Grade}
+#'     \item{friendship_7th}{7th Grade}
+#'     \item{friendship_8th}{8th Grade}
+#' }
+#'
+#'
+#' @docType data
+#'
+#'
+"pupils_classroom"
