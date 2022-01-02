87ddc0f5a74419d11efd2b915026b60c4219bd1f
Alejandro Espinosa <aespinos@gess-rz-dock-1-085.ethz.ch>
2022-01-02 17:36:18 +0100

    Typo

--- R/southern_women.R
+++ R/southern_women.R
@@ -1,6 +1,6 @@
 #' Southern Women
 #'
-#' This data was collected in 1936 for ethnographic studies performed by A. Davis, B.B. Gardner and R. Gardner, together with Elizabeth Stubbs Davis and J.G.St. Clair Drake. From the so called "First Harvard School of Networks" (Freeman, 2004). It consists of two incident matrices. The first matrix is an informal group of 18 women attending 14 events. The second matrix is a different group of 6 women attending 9 events.
+#' This data was collected in 1936 from ethnographic studies performed by A. Davis, B.B. Gardner and R. Gardner, together with Elizabeth Stubbs Davis and J.G.St. Clair Drake. From the so called "First Harvard School of Networks" (Freeman, 2004). It consists of two incident matrices. The first matrix is an informal group of 18 women attending 14 events. The second matrix is a different group of 6 women attending 9 events.
 #' The dates and social events were reported in Old City Herald and the names were reported utilizing interviews, the record of participant-observers and guest lists. The two groups are divided according to race categories, and the cliques are differentiated between core, primary and secondary members within one of the groups.
 #'
 #' @references Freeman, Linton C. (2004). The Development of Social Network Analysis. A Study in the Sociology of Science. Empirical Press, Vancouver, BC Canada
