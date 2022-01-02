41b17b7b0aed820d1a46a1181bdbea36dc1b59e3
Alejandro Espinosa <aespinos@gess-rz-dock-1-085.ethz.ch>
2022-01-02 17:34:18 +0100

    Add documentation EIES

--- R/eies.R
+++ R/eies.R
@@ -1,28 +1,30 @@
-#' The Electronic Information Exchange System (EIES) 
-#' 
-#' The data were collected by Freeman and Freeman (1980). 
-#' This is a network of researchers who participated in an early study on 
-#' the effects of electronic information exchange, 
+#' The Electronic Information Exchange System (EIES)
+#'
+#' The data were collected by Freeman and Freeman (1980).
+#' This is a network of researchers who participated in an early study on
+#' the effects of electronic information exchange,
 #' a precursor of email communication.
-#' 
+#'
 #' @format A 32 X 32 matrix.
 #' \describe{
-#'     \item{time1}{0 = do not know the other, 
-#'     1 = heard about the other, did not meet him/her, 
-#'     2 = have met the other, 
-#'     3 = friend, 
+#'     \item{time1}{January 1978. 0 = do not know the other,
+#'     1 = heard about the other, did not meet him/her,
+#'     2 = have met the other,
+#'     3 = friend,
 #'     4 = close personal friend.}
-#'     \item{time2}{0 = do not know the other, 
-#'     1 = heard about the other, did not meet him/her, 
-#'     2 = have met the other, 
-#'     3 = friend, 
+#'     \item{time2}{September 1978. 0 = do not know the other,
+#'     1 = heard about the other, did not meet him/her,
+#'     2 = have met the other,
+#'     3 = friend,
 #'     4 = close personal friend.}
 #'     \item{label}{The regins in which each Lozi king reigned.
 #'     It was copy exactly from the original source. 0: no information.}
-#'     \item{citations}{Number of citations at the beginning of the study}
-#'     \item{discipline}{1 = sociology, 
-#'     2 = anthropology, 
-#'     3 = mathematics/statistics, 
+#'     \item{citations}{Number of citations at the beginning of the study
+#'     extracted from the Social Science Index}
+#'     \item{discipline}{Data extracted from a questionnaire, in which
+#'     1 = sociology,
+#'     2 = anthropology,
+#'     3 = mathematics/statistics,
 #'     4 = psychology/communication.}
 #' }
 #'
