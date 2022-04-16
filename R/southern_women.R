c5a2a4791313e9ee13acfb56b0289b76d6d1b2e9
anespinosa <anespinosa@uc.cl>
2022-04-16 11:39:32 +0200

    Reference Southern Women

--- R/southern_women.R
+++ R/southern_women.R
@@ -3,7 +3,11 @@
 #' This data was collected in 1936 from ethnographic studies performed by A. Davis, B.B. Gardner and R. Gardner, together with Elizabeth Stubbs Davis and J.G.St. Clair Drake. From the so called "First Harvard School of Networks" (Freeman, 2004). It consists of two incident matrices. The first matrix is an informal group of 18 women attending 14 events. The second matrix is a different group of 6 women attending 9 events.
 #' The dates and social events were reported in Old City Herald and the names were reported utilizing interviews, the record of participant-observers and guest lists. The two groups are divided according to race categories, and the cliques are differentiated between core, primary and secondary members within one of the groups.
 #'
-#' @references Freeman, Linton C. (2004). The Development of Social Network Analysis. A Study in the Sociology of Science. Empirical Press, Vancouver, BC Canada
+#' @references
+#'
+#' Freeman, Linton C. (2004). The Development of Social Network Analysis. A Study in the Sociology of Science. Empirical Press, Vancouver, BC Canada
+#'
+#' Freeman, Linton C. (2003). Finding Social Groups: A Meta-Analysis of the Southern Women Data. In: Ronald Breiger, Kathleen Carley, and Philippa Pattison (Eds). Dynamic Social Network Modeling and Analysis: Workshop Summary and Papers. The National Academy of Science.
 #'
 #' @format A list of three incident matrices
 #' \describe{
@@ -16,7 +20,7 @@
 #' @source Davis, Allison; Gardner, Burleigh B. and Mary. R. Gardner (1941). Deep South: A Social Anthropological Study of Caste and Class. The University of Chicago Press, Chicago.
 #'
 #' @details
-#' In the second version of the group 1 (i.e., \code{group1b}) four links are added (Dorothy, 6/10; Dorothy, 4/7; Helen, 11/21; Helen, 8/3)
+#' In the second version of the group 1 (i.e., \code{group1b} from \code{membership_group1}) four links are added (Dorothy, 6/10; Dorothy, 4/7; Helen, 11/21; Helen, 8/3).
 #'
 #' @usage data(southern_women)
 #'
